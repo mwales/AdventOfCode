ee0629cb50bf7cad1b5f0dba7dd7dc08ad832b7d
Michael Wales <mwales3@gmail.com>
2023-12-02 00:37:19 -0500

    AoC 2023 day 2

---
+++ 2023/day2/p2.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import sys
+
+def debug(msg):
+	if True:
+		print(msg + "\n")
+
+def addAllCubes(msgData):
+	retVal = { "red": 0,
+	           "blue": 0,
+	           "green": 0 }
+
+	parts = msgData.split(":")
+	goodParts = parts[1]
+	
+	hands = goodParts.split(";")
+	for curHand in hands:
+		debug(f"curHand = {curHand}")
+		handParts = curHand.split(" ")[1:]
+
+		for i in range(0, len(handParts), 2):
+			debug(f"handParts = {handParts}")
+			#debug(f"Count = {goodParts[i]} = {goodParts[i+1]}")
+			count = int(handParts[i])
+			color = handParts[i+1].replace(",","")
+
+			for curColor in retVal:
+				if (color == curColor) and (retVal[curColor] < count):
+					retVal[curColor] =count
+
+	return retVal
+
+def main(argv):
+	
+	if (len(argv) < 2):
+		print("Usage: {} inputfile".format(sys.argv[0]))
+		return
+	
+	f = open(argv[1])
+	data = f.read().strip().split("\n")
+	f.close()
+
+	bagLimits = { "red": 12,
+	              "green": 13,
+	              "blue": 14 }
+
+	gameSum = 0
+	for i in range(len(data)):
+		gameId = i + 1
+		retDict = addAllCubes(data[i])
+		debug(f"REsult = {retDict}")
+
+		powerVal = retDict["red"] * retDict["green"] * retDict["blue"]
+
+		debug(f"Power {powerVal} works!")
+		gameSum += powerVal
+
+	print(f"{gameSum}")
+
+if __name__ == "__main__":
+	main(sys.argv)
