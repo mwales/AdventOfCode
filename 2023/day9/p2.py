2607dd004acfdf1e7673a404244297bd49455517
Michael Wales <mwales3@gmail.com>
2023-12-09 17:13:49 -0500

    AoC 2023 day 9... 44kth place cause i stayed up and watched a movie instead of AoC

---
+++ 2023/day9/p2.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import sys
+
+def debug(msg):
+	if True:
+		print(msg)
+
+def solveSeq(numSeq):
+	# Is sequence all zeros?
+	allZeros = True
+	for sn in numSeq:
+		if (sn != 0):
+			allZeros = False
+			break
+	
+	if allZeros:
+		return 0
+
+	# If we get to this point, we are not at the end stage.  Find
+	# all the differences and then call ourselves
+	childSeq = []
+	for i in range(len(numSeq) - 1):
+		childSeq.append(numSeq[i+1] - numSeq[i])
+	
+	addToMe = solveSeq(childSeq)
+
+	retVal = numSeq[0] - addToMe
+	debug(f"For seq {numSeq}, we added {retVal} to the front")
+	return retVal
+
+def main(argv):
+	
+	if (len(argv) < 2):
+		print("Usage: {} inputfile".format(sys.argv[0]))
+		return
+	
+	f = open(argv[1])
+	data = f.read().strip().split("\n")
+	f.close()
+
+	sumSolution = 0
+	for eachSeq in data:
+		numSeq = [ int(x) for x in eachSeq.split() ]
+		sumSolution += solveSeq(numSeq)
+
+	print(sumSolution)
+if __name__ == "__main__":
+	main(sys.argv)
